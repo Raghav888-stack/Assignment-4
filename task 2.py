7f9f21421f07ea04ec01219c2f05c24616fdbc42
Raghav888-stack <raghavachess99@gmail.com>
2025-09-26 14:12:26 +0530

    Update task 2.py

--- task 2.py
+++ task 2.py
@@ -1,9 +1,9 @@
 a = input('Enter text to write to the file:')
 with open('sample.txt','w')as file1:
     appending_file = file1.write(a)
-    print("Data successfully written to output.txt.")
+    print("Data successfully written to sample.txt.")
     b = input('Enter additional text to append:')
-    with open('sample.text','a')as file1:
+    with open('sample.txt','a')as file1:
         appending_file = file1.write("\n"+b)
         print('Data succesfully appended.')
         print("Final content of output.txt:")
